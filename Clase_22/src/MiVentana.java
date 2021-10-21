82478344c1e05cf2ff6f9dc452007829c9facd1b
Luciano Diamand <lucianodiamand@yahoo.com>
2021-10-21 19:15:28 -0300

    Agrega los ejemplos de alineamiento de componentes

---
+++ Clase_22/src/MiVentana.java
@@ -0,0 +1,56 @@
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.GridLayout;
+import java.awt.LayoutManager2;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class MiVentana extends JFrame {
+
+	public MiVentana() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(800, 800);
+		setVisible(true);
+		
+		Container ctx = getContentPane();
+//		JPanel menu = new JPanel();
+//		menu.setLayout(new GridLayout(3, 3));
+		
+		JPanel botonera = new JPanel();
+		LayoutManager2 lo = new BoxLayout(botonera, BoxLayout.Y_AXIS);
+		botonera.setLayout(lo);
+		botonera.setAlignmentY(Component.CENTER_ALIGNMENT);
+		
+		JButton button1 = new JButton("Boton 1");
+		button1.setAlignmentX(Component.CENTER_ALIGNMENT);
+		botonera.add(button1);
+		JButton button2 = new JButton("Boton 2");
+		button2.setAlignmentX(Component.CENTER_ALIGNMENT);
+		botonera.add(button2);
+		JButton button3 = new JButton("Boton 3");
+		button3.setAlignmentX(Component.CENTER_ALIGNMENT);
+		button3.setAlignmentY(Component.CENTER_ALIGNMENT);
+		botonera.add(button3);
+		
+//		menu.add(new JButton());
+//		menu.add(new JButton());
+//		menu.add(new JButton());
+//		menu.add(new JButton());
+//		menu.add(botonera);
+//		menu.add(new JButton());
+//		menu.add(new JButton());
+//		menu.add(new JButton());
+//		menu.add(new JButton());
+		
+		ctx.add(botonera, BorderLayout.CENTER);
+	}
+	
+	public static void main(String[] args) {
+		MiVentana mv = new MiVentana();
+	}
+}
