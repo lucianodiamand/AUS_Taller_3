11adc30d85fa87e4b53be9aa95299f1b85b9cacd
Luciano Daniel Diamand <lucianodiamand@gmail.com>
2025-09-03 20:27:35 -0300

    Add uninitialized local variable

--- clase01/src/PersonaDemo.java
+++ clase01/src/PersonaDemo.java
@@ -3,7 +3,10 @@ public class PersonaDemo {
 
     public static void main(String [] args) {
         Persona p1 = new Persona();
-	p1.cambiarEdad(40);
-	System.out.println(p1.obtenerEdad());
+        p1.cambiarEdad(40);
+        System.out.println(p1.obtenerEdad());
+
+        Integer i;
+        // System.out.println(i); // Error
     }
 }
